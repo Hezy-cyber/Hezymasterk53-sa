d8ed21294015ac907dfad9a247aa5f6e7c1f2bf4
Hezy-cyber <hezekielhezy@gmail.com>
2026-07-12 09:48:54 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
-function showMessage(code){
-    alert("Welcome to " + code + " practice!");
+function showMessage(code) {
+    if (code === "Code 1 - Motorcycle") {
+        window.location.href = "code1.html";
+    } else if (code === "Code 2 - Light Motor Vehicle") {
+        window.location.href = "code2.html";
+    } else if (code === "Code 3 - Heavy Motor Vehicle") {
+        window.location.href = "code3.html";
+    }
 }
